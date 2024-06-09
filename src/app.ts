db52def06ba946d438849e23ecc641db060331fd
ismael <maelio@live.com>
2024-06-08 21:27:03 -0300

    Implementação da Rota de Cadastro de Usuários
    Adicionada a primeira rota para o cadastro de usuários, permitindo testar a integração com o banco de dados. Também foi criado o arquivo client.http para facilitar os testes da API diretamente dentro do VSCode. Essa configuração inicial é fundamental para validar a funcionalidade da conexão com o banco de dados e a correta recepção de dados através da API.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-// const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    password: z.string().min(3),
+    status: z.enum(['ACTIVE', 'INACTIVE']).default('ACTIVE'),
+    sectorId: z.number().default(1),
+  })
+  const { name, password, status, sectorId } = registerBodySchema.parse(
+    request.body,
+  )
+
+  await prisma.user.create({
+    data: {
+      name,
+      status,
+      password_hash: password,
+      sector_id: sectorId,
+    },
+  })
+  return reply.status(201).send()
+})
